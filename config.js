8384645b83bd6e975a70a83388b6c404f28fbb3f
lehoangan1208 <91781094+lehoangan1208@users.noreply.github.com>
2021-10-02 16:59:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Chị à!',
+    introDesc: `Làm nông thì có quả
+    Làm việc thì có tiền
+    Thế em làm gì để có chị 😙`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'Vẫn cứ là thích chị <33',
+    btnNo: 'Chúng mình không quen nhau!!!!',
+    question:'Trăng rằm có bánh trung thu. Tiện cho em hỏi gu chị là gì???',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Gu của chị chính là 😘',
+    mess: 'EM 🥰',
+    messDesc: 'Hông biết em có thể hẹn chị một buổi cafe được hông?',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
